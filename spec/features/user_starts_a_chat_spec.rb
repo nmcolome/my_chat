330ddd51a69e22a445ac37712db66a763873de15
NataliaColome <nmcolome@gmail.com>
2017-07-27 21:59:50 -0600

    create user_starts_a_chat_spec

---
+++ spec/features/user_starts_a_chat_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "a user starts a conversation" do
+  it "can chat with another user" do
+    user = User.create(username: "test_user", password: "password")
+    user2 = User.create(username: "test_user2", password: "password")
+    allow(SessionsController).to receive(:current_user).and_return(user)
+
+    visit "/"
+    click_on "Chat"
+    fill_in :input, with: "Test message"
+    click_on "Send"
+
+    expect(page).to have_content("Test message")
+    expect(page).to have_content("#{user.username}:")
+  end
+end
