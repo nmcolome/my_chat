09d0c400e9123bcee9ecae21c436faed0cf653cb
NataliaColome <nmcolome@gmail.com>
2017-07-27 22:30:03 -0600

    fix test field_names of form

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe "a user can signup" do
     click_on "signup"
 
     expect(current_path).to eq("/users/new")
-    fill_in :username, with: "test_user"
-    fill_in :password, with: "password"
+    fill_in "Username", with: "test_user"
+    fill_in "Password", with: "password"
     click_on "Sign Up"
 
     expect(current_path).to eq("/")
+    expect(User.count).to eq(1)
   end
 end
