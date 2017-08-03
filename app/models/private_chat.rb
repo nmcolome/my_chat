fe6e4a93900be971dc039ccdc5c7fbe910c2145a
NataliaColome <nmcolome@gmail.com>
2017-08-03 00:46:32 -0600

    generate model, views and controller for PrivateChat. A user can select in the private_chat#new view all the users he wants to have a conversation with, we take it as an array, we add the current_user id and sort it. The new conversation is saved and redirected to the show view

---
+++ app/models/private_chat.rb
@@ -0,0 +1,7 @@
+class PrivateChat < ApplicationRecord
+  before_save :canonicalize_ids
+
+  def canonicalize_ids
+    self.participant_ids = self.participant_ids.map {|v| v.to_i }.uniq.sort
+  end
+end
