9a53e4f0a126dd4630076b2ebe6f1cf3c262d724
NataliaColome <nmcolome@gmail.com>
2017-08-03 00:43:40 -0600

    generate migration for private_chat table with participant_ids field as an array

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728041654) do
+ActiveRecord::Schema.define(version: 20170803052248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "private_chats", force: :cascade do |t|
+    t.string "participant_ids", default: [], array: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
